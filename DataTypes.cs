046d0b87764a7bb5d20fb1f82cc365b6db7a9d00
Prince3963 <princepatel3963@gmail.com>
2026-01-21 14:29:34 +0530

    Add project files.

---
+++ DataTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1st_day
+{
+    public class DataTypes
+    {
+        public void ShowDataTypes()
+        {
+            // Integer data type
+            int intValue = 42;
+            Console.WriteLine($"Integer Value: {intValue}");
+
+            // Floating-point data type
+            double doubleValue = 3.14159;
+            Console.WriteLine($"Double Value: {doubleValue}");
+
+            // Character data type
+            char charValue = 'A';
+            Console.WriteLine($"Character Value: {charValue}");
+
+            // String data type
+            string stringValue = "Hello, World!";
+            Console.WriteLine($"String Value: {stringValue}");
+
+            // Boolean data type
+            bool boolValue = true;
+            Console.WriteLine($"Boolean Value: {boolValue}");
+        }
+    }
+}
